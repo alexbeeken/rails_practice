a898624ea4e1fcb4e96490d9c98734de14e88a31
Alex Beeken <aeb242@gmail.com>
2015-03-15 14:17:32 -0700

    Have Edit Form For Items On List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,28 @@ class ListsController < ApplicationController
     render :index
   end
 
+  def new
+    @list = List.new(:name => params[:list])
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      # use redirect_to after succeffully CRUDing an object
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
   def show
     @list = List.find(params[:id])
     render :show
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
   end
 end
